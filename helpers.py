e8e73f5551bf5be6dd301bdedfb7d786627eb17e
tonypnode <sgtp@me.com>
2017-10-18 21:35:49 -0400

    Create helpers.py
    Initial Production

---
+++ helpers.py
@@ -0,0 +1,14 @@
+import re
+
+
+def macaddr_parse(mac_string, chunk=3, delimiter='-', upper=True):
+    """ Parses any usual MAC address combination per configured params"""
+    wash = re.sub(r'[^A-Fa-f0-9]+', '', mac_string)
+    if len(wash) != 12:
+        raise ValueError('Incorrect MAC Address format: Should be 12 Hex chars')
+    else:
+        dry = delimiter.join((wash[i:i+chunk] for i in range(0, len(wash), chunk)))
+        if upper:
+            return dry.upper()
+        else:
+            return dry
